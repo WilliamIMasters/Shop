57c10c31831b1b63dd8a970df25e703539c2d62a
William Masters <williamianm@hotmail.com>
2018-02-24 22:02:22 +0000

    Server
    Added nodejs server with static html, css and js

---
+++ website/node.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+   res.sendFile(__dirname + "/" + "public/home.html");
+})
+
+var server = app.listen(8080, function() {
+   var host = server.address().address;
+   var port = server.address().port;
+});
+
+var server = app.listen(80, function() {
+   var host = server.address().address;
+   var port = server.address().port;
+});
---
+++ website/public/scripts/bannerBackground.js
@@ -0,0 +1,21 @@
+var canvas;
+var canvasContext;
+let r = 0;
+let g = 0;
+let b = 0;
+
+window.onload = function() {
+   canvas = document.getElementById("bannerC");
+   canvasContext = canvas.getContext("2d");
+   canvasContext.fillStyle = "#EA3D77";
+   canvasContext.fillRect(0,0, canvas.width, canvas.height);
+
+   animate();
+
+}
+
+function animate() {
+
+   requestAnimationFrame(animate);
+}
+//https://ayylienclothing.com
